c10ac90de84a07d80e7dcf268e1c172b8acc300e
CobyPear <pear.coby@gmail.com>
2022-12-02 21:48:48 -0600

    Solve part 2

--- 2022/day2/rps-tournament-cheat.mjs
+++ 2022/day2/rps-tournament-cheat.mjs
@@ -40,11 +40,66 @@ const rpsCheatManualReader = (manual) => {
 
         } else {
             // round is a loss (0 pts)
-            return myChoice || 0
+            return myChoice
         }
     })
     return scores.reduce((a,c) => a + c)
 }
+const rpsCheatManualReaderVersion2 = (manual) => {
+    // A - rock
+    // B - paper
+    // C - scissors
+    // X - lose
+    // Y - draw
+    // Z - win
+    const r = 1 
+    const p = 2 
+    const s = 3
+    const dict = {
+        A: r,
+        B: p,
+        C: s,
+        X: 'lose',
+        Y: 'draw',
+        Z: 'win',
+    }
+    
+    let scores = manual.split('\n')
+        // remove empty line
+        scores.pop()
+        scores = scores.map((line) => {
+       const [opp, me] = line.split(' ')
+        const oppChoice = dict[opp]
+        const myChoice = dict[me]
+            switch (myChoice) {
+                case 'lose':
+                    switch (oppChoice){
+                        case r:
+                            return s
+                        case p:
+                            return r
+                        case s:
+                            return p
+                    }
+                case 'draw':
+                    return oppChoice + 3
+                case 'win':
+                    switch (oppChoice){
+                        case r:
+                            return p + 6
+                        case p:
+                            return s + 6
+                        case s:
+                            return r + 6
+                    }
+                default:
+                    throw 'Something went wrong...'
 
+            }
+
+    })
+    return scores.reduce((a,c) => a + c)
+}
 
 console.log(rpsCheatManualReader(rawFile))
+console.log(rpsCheatManualReaderVersion2(rawFile))
