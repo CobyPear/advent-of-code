03e7f43b8477937e85010715ff7bd7da0b39af22
CobyPear <pear.coby@gmail.com>
2024-12-01 10:51:45 -0600

    Solve day1 part2
    Co-authored-by: aspitz1-ml <anna.spitz@modernlogic.io>

--- 2024/day1/historian-hysteria.mjs
+++ 2024/day1/historian-hysteria.mjs
@@ -13,6 +13,7 @@ const makeArrays = (input) => {
     return [a, b]
 }
 
+//part 1
 // calculate the distance
 const calculateDistance = (arrays) => {
     const [a, b] = arrays
@@ -25,4 +26,18 @@ const calculateDistance = (arrays) => {
     }, 0)
 }
 
-console.log(calculateDistance(makeArrays(rawFile)))
+// part 2
+const calculateSimilarityScore = (arrays) => {
+    const [a, b] = arrays
+    // multiply a for each time it occurs in list b
+    return a.map((n) => {
+        const numToMultiplyBy = b.filter(x => x === n).length
+        return numToMultiplyBy * n
+    }).reduce((acc,cur)=>{
+        acc = acc + cur
+        return acc
+    }, 0)
+}
+
+// console.log(calculateDistance(makeArrays(rawFile)))
+console.log(calculateSimilarityScore(makeArrays(rawFile)))
