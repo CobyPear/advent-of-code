5afb8bf6f1df9b8ee4f4c6b40a3258c164396516
CobyPear <pear.coby@gmail.com>
2024-12-08 08:44:00 -0600

    Solve day3 part1

---
+++ 2024/3/mull-it-over.mjs
@@ -0,0 +1,34 @@
+import inputFileHelper from "../../utils/input-file-helper.js";
+
+const sampleInput = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`;
+const rawFile = inputFileHelper(process.argv[2]).trim();
+
+const getMuls = (input) => {
+  // get the muls
+  const regexp = /(?:mul\(\d+,\d+\))/g;
+  const matches = input.match(regexp);
+
+  if (matches.length) {
+    console.log(matches);
+    // map over the muls and get the numbers,
+    const results = matches
+      .map((m) => {
+        const matches = m.matchAll(/(?:\()(?<one>\d+)(?:,)(?<two>\d+)(?:\))/g);
+        for (const match of matches) {
+          console.log(match);
+          const [, mul1, mul2, ...rest] = match;
+          // multipy them
+          return parseInt(mul1) * parseInt(mul2);
+        }
+      })
+      .reduce((acc, cur) => {
+        // add the results
+        acc = acc + cur;
+        return acc;
+      }, 0);
+
+    console.log(results);
+  }
+};
+
+console.log(getMuls(rawFile));
