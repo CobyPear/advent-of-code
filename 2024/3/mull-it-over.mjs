cf1e938686832b7b92f5bfa534e28ae72d5a47f8
CobyPear <pear.coby@gmail.com>
2024-12-08 09:50:30 -0600

    Solve day3 part2

--- 2024/3/mull-it-over.mjs
+++ 2024/3/mull-it-over.mjs
@@ -1,14 +1,15 @@
 import inputFileHelper from "../../utils/input-file-helper.js";
 
 const sampleInput = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`;
+const sampleInput2 = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`;
 const rawFile = inputFileHelper(process.argv[2]).trim();
 
 const getMuls = (input) => {
-  // get the muls
-  const regexp = /(?:mul\(\d+,\d+\))/g;
+  // get the muls (got some hits on the regex for part 2 :))
+  const regexp = /((?<=(?:do\(\)|^)(?:[^d]|d(?!on't\(\)))*)(mul\(\d+,\d+\)))/g;
   const matches = input.match(regexp);
 
-  if (matches.length) {
+  if (matches?.length) {
     console.log(matches);
     // map over the muls and get the numbers,
     const results = matches
